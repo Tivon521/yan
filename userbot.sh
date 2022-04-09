7df9b34b791e4e60c84de3e71775e4e43830c337
Tivon <62788268+Tivon521@users.noreply.github.com>
2022-04-09 15:15:35 +0800

    Update userbot.sh

--- userbot.sh
+++ userbot.sh
@@ -3,4 +3,4 @@ echo -e "======================== 1. 部署自定义jbot-diy机器人 ==========
 if [ -d "/jd" ]; then root=/jd; else root=/ql; fi; if [ -f $root/diybot.sh ]; then rm -f $root/diybot.sh; fi; cd $root; wget https://raw.githubusercontent.com/msechen/JD_Diy/main/shell/diybot.sh; bash diybot.sh
 
 echo -e "======================== 2. 部署[user.py]监控机器人 ========================\n"
-if [ -d "/jd" ]; then root=/jd; else root=/ql; fi; if [ -f $root/user.sh ]; then rm -f $root/user.sh; fi; cd $root; wget https://raw.githubusercontent.com/msechen/JD_Diy/main/shell/user.sh; bash user.sh
+if [ -d "/jd" ]; then root=/jd; else root=/ql; fi; if [ -f $root/user.sh ]; then rm -f $root/user.sh; fi; cd $root; wget https://raw.githubusercontent.com/Tivon521/yan/ziyong/user.sh; bash user.sh
